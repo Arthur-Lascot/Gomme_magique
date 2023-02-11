4cef343a478b74f51166b4fe72f94048a2d95038
Oldrati <arthur.oldrati@epita.fr>
2023-02-11 11:17:03 +0100

    Added some tools from sdl1

--- src/tools.c
+++ src/tools.c
@@ -15,6 +15,89 @@ SDL_Surface* load_image(const char* path)
     SDL_Surface* surface= IMG_Load(path);
     return SDL_ConvertSurfaceFormat(surface, SDL_PIXELFORMAT_RGB888,0);
 }
+
+void wait_for_keypressed()
+{
+    SDL_Event event;
+
+    // Wait for a key to be down.
+    do
+    {
+        SDL_PollEvent(&event);
+    } while(event.type != SDL_KEYDOWN);
+
+    // Wait for a key to be up.
+    do
+    {
+        SDL_PollEvent(&event);
+    } while(event.type != SDL_KEYUP);
+}
+
+Uint8* pixel_ref(SDL_Surface *surf, unsigned x, unsigned y)
+{
+    int bpp = surf->format->BytesPerPixel;
+    return (Uint8*)surf->pixels + y * surf->pitch + x * bpp;
+}
+
+Uint32 get_pixel(SDL_Surface *surface, unsigned x, unsigned y)
+{
+    Uint8 *p = pixel_ref(surface, x, y);
+
+    switch (surface->format->BytesPerPixel)
+    {
+        case 1:
+            return *p;
+
+        case 2:
+            return *(Uint16 *)p;
+
+        case 3:
+            if (SDL_BYTEORDER == SDL_BIG_ENDIAN)
+                return p[0] << 16 | p[1] << 8 | p[2];
+            else
+                return p[0] | p[1] << 8 | p[2] << 16;
+
+        case 4:
+            return *(Uint32 *)p;
+    }
+
+    return 0;
+}
+
+void put_pixel(SDL_Surface *surface, unsigned x, unsigned y, Uint32 pixel)
+{
+    Uint8 *p = pixel_ref(surface, x, y);
+
+    switch(surface->format->BytesPerPixel)
+    {
+        case 1:
+            *p = pixel;
+            break;
+
+        case 2:
+            *(Uint16 *)p = pixel;
+            break;
+
+        case 3:
+            if(SDL_BYTEORDER == SDL_BIG_ENDIAN)
+            {
+                p[0] = (pixel >> 16) & 0xff;
+                p[1] = (pixel >> 8) & 0xff;
+                p[2] = pixel & 0xff;
+            }
+            else
+            {
+                p[0] = pixel & 0xff;
+                p[1] = (pixel >> 8) & 0xff;
+                p[2] = (pixel >> 16) & 0xff;
+            }
+            break;
+
+        case 4:
+            *(Uint32 *)p = pixel;
+            break;
+    }
+}
 //Uint32* pixels = surface->pixels;
 //SDL_GetRGB(pixels[j+i*h], format, &r, &g, &b);
 /*if (SDL_Init(SDL_INIT_VIDEO) != 0)
