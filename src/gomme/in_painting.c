ba05f887efbaed12bd2c7366555732598f26deb1
N1c0 <48924079+N1C02@users.noreply.github.com>
2023-04-01 22:40:58 +0200

    *add in_painting.h OK *fill in_painting.c with almost all needed methods for one step of inPainting algorithm * all methods have doxygen

--- src/gomme/in_painting.c
+++ src/gomme/in_painting.c
@@ -1,23 +1,257 @@
-void inPainting(SDL_Surface surface, int* map)
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include "in_painting.h"
+#include "../STP/tools.h"
+
+/// @brief Set true in ret[0] if the entier area psi is in the bound of image AND set x in ret[1] and y in ret[2] (the upper left corner, not the center)
+/// @param pixel 
+/// @param w Width
+/// @param h Height
+/// @param ret tab of return : {is valid ?, x, y}
+void is_valid(int pixel, int w, int h, int *ret)
+{
+    int x = pixel % w;
+    int y = pixel / w;
+    int offset = (PSY_W - 1) / 2;
+
+    x -= offset;
+    y -= offset;
+
+    ret[0] = x<0 || y<0 || x+PSY_W>w || y+PSY_W>h;
+    ret[1] = x;
+    ret[2] = y;
+}
+
+/// @brief Compute confidence for pixel
+/// @param pixel 
+/// @param C Tab of confidence
+/// @param w Width
+/// @param h Height
+/// @return Confidence for pixel
+double conf(int pixel, double* C, int w, int h)
+{
+    int arg[3];
+    is_valid(pixel,w,h,arg);
+
+    if (arg[0])
+        return 0.0;
+
+    int x = arg[1];
+    int y = arg[2];
+
+    double confidence = 0.0;
+
+    int max = (y + PSY_W) * w + (x + PSY_W);
+    for (int i = y*w+x; i < max; i++)
+    {
+        confidence += C[i];
+
+        if (i%w == x+PSY_W)
+            i += w - PSY_W;
+    }
+    confidence /= PSY_W * PSY_W;
+
+    return confidence;
+}
+
+/// @warning TODO
+/// @brief Compute data term, D(p) in math formula
+/// @return data term
+double data_term()
+{
+    return 1.0;
+}
+
+/// @warning /!\ double type not sure /!\
+/// @brief Compute the distance between  psi of p  and  psi of q
+/// @param surface The image sdl surface's
+/// @param p Point with the best P(p)
+/// @param q Point to test with p to check distance between their psi
+/// @param map Tab of status (0:source, 1:border, 2:target)
+/// @return The disance
+double dist_psi(SDL_Surface *surface, int p, int q, int* map)
 {
     int w = surface->w;
     int h = surface->h;
-    int C[w*h];
+
+    int arg[3];
+    is_valid(q,w,h,arg);
+
+    if (arg[0])
+        return __DBL_MAX__;
+
+    int x = arg[1];
+    int y = arg[2];
+
+    int max = (y + PSY_W) * w + (x + PSY_W);
+    for (int i = y*w+x; i < max; i++)
+    {
+        if (map[i] != 0) // if not in source img
+            return __DBL_MAX__;
+
+        if (i%w == x+PSY_W) // jump line
+            i += w - PSY_W;
+    }
+
+    //TODO ssd
+    return (double)(p - q);//FAKE
+}
+
+/// @brief Set the new borders in map at one larger pixel side around
+/// @param map Tab of status (0:source, 1:border, 2:target)
+/// @param x Upper left x coordinate from p in copy() method
+/// @param y Upper left y coordinate from p in copy() method
+/// @param w Width
+/// @param h Height
+void update_border(int* map, int x, int y, int w, int h)
+{
+    x--;
+    y--;
+ 
+    int max = PSY_W + 2;
+
+    if (y<0)
+        x += max - 1;
+    else
+        for (int i = 0; i < max; i++) {
+            if (!(map[y * w + x]==0 || x<0 || x+PSY_W>w))
+                map[y * w + x] = 1;
+            x++;
+        }
+    
+    if (x+PSY_W>w)
+        y += max - 1;
+    else
+        for (int i = 0; i < max; i++) {
+            if (!(map[y * w + x]==0 || y<0 || y+PSY_W>h))
+                map[y * w + x] = 1;
+            y++;
+        }
+    
+    if (y+PSY_W>h)
+        x -= max - 1;
+    else
+        for (int i = 0; i < max; i++) {
+            if (!(map[y * w + x]==0 || x<0 || x+PSY_W>w))
+                map[y * w + x] = 1;
+            x--;
+        }
+    
+    if (x<0)
+        return; // y -= max - 1;
+    else
+        for (int i = 0; i < max; i++) {
+            if (!(map[y * w + x]==0 || y<0 || y+PSY_W>h))
+                map[y * w + x] = 1;
+            y--;
+        }
+}
+
+/// @brief Copy pixels of q psi in p psi and update map and C tabs
+/// @param surface The image sdl surface's
+/// @param C Tab of confidence
+/// @param map Tab of status (0:source, 1:border, 2:target)
+/// @param p Point with the best P(p)
+/// @param q Point with the minimal distance of psi to p psi
+/// @param conf_p Confidence of p psi filling
+void copy(SDL_Surface *surface, double *C, int *map, int p, int q, double conf_p)
+{
+    int offset = (PSY_W - 1) / 2;
+    int w = surface->w;
+    int h = surface->h;
+    int xp = p % w;
+    int yp = p / w;
+    xp -= offset;
+    yp -= offset;
+
+    int xq = q % w;
+    int yq = q / w;
+    xq -= offset;
+    yq -= offset;
+
+    int max = (yp + PSY_W) * w + (xp + PSY_W);
+    int ip = yp*w+xp;
+    int iq = yq*w+xq;
+
+    Uint32 pixel;
+    int pline;
+    int pcolumn;
+    int qline;
+    int qcolumn;
+    while (ip < max /*&& iq < maxq*/)
+    {
+        if (map[ip] != 0) {
+            C[ip] = conf_p;
+            map[ip] = 0;
+
+            pline = ip / w;
+            pcolumn = ip % w;
+            qline = iq / w;
+            qcolumn = iq % w;
+            pixel = get_pixel(surface, qcolumn, qline);
+            put_pixel(surface, pcolumn, pline, pixel);
+        }
+
+        if (ip%w == xp+PSY_W) // jump line
+            ip += w - PSY_W;
+
+        ip++;
+        iq++;
+    }
+    update_border(map, xp, yp, w, h);
+}
+
+/// @brief One step of inPainting algorithm
+/// @param surface The image sdl surface's
+/// @param map Tab of status (0:source, 1:border, 2:target)
+void inPainting(SDL_Surface *surface, int* map)
+{
+    const int w = surface->w;
+    const int h = surface->h;
+    const int len = w * h;
+    double C[len];
     //initialization of the array of confidence
-    for(int i = 0; i<w*h; i++)
+    for(int i = 0; i<len; i++)
     {
         if(map[i]==0)
             C[i] = 1; // C(pixel) = 1 for pixel in (I - Omega)
         else
             C[i] = 0; // C(pixel) = 0 for pixel in (Omega)
-    }// note à moi même avoir une aire superficiel de vrai aire + 1 contour de pixel pour bouger la bordure
-    
+    }
+
+    double tmp;
+    double tmp_conf;
+    double max = 0.0;
+    double max_conf = 0.0;
+    double max_p;
     // finding the priority values
-    for(int i = 0; i<w*h; i++)
+    for(int p = 0; p<len; p++)
     {
-        if(map[i]==1)// points of the edge
+        if(map[p]==1)// points of the edge
         {
-            
+            tmp_conf = conf(p, C, w, h);
+            tmp = tmp_conf; //* data_term() TODO
+            if (tmp>max) {
+                max = tmp;
+                max_p = p;
+                max_conf = tmp_conf;
+            }
         }
     }
+
+    //double tmp;
+    double min = __DBL_MAX__;
+    double min_d;
+    for(int q = 0; q<len; q++)
+    {
+        if(map[q]==0) {
+            tmp = dist_psi(surface, max_p, q, map);
+            if (tmp<min) {
+                min = tmp;
+                min_d = q;
+            }
+        }
+    }
+
+    copy(surface, C, map, max_p, min_d, max_conf);
 }
