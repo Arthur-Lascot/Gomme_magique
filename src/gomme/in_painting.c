5cc48e21b1a665f1cd6475ecfd4b2134540e950c
Oldrati <arthur.oldrati@epita.fr>
2023-04-01 15:46:02 +0200

    Initialization of C

--- src/gomme/in_painting.c
+++ src/gomme/in_painting.c
@@ -3,11 +3,12 @@ void inPainting(SDL_Surface surface, int* map)
     int w = surface->w;
     int h = surface->h;
     int C[w*h];
+    //initialization of the array of confidence
     for(int i = 0; i<w*h; i++)
     {
         if(map[i]==0)
-            C[i] = 1;
+            C[i] = 1; // C(pixel) = 1 for pixel in (I - Omega)
         else
-            C[i] = 0;
-    }
+            C[i] = 0; // C(pixel) = 0 for pixel in (Omega)
+    }// note à moi même avoir une aire superficiel de vrai aire + 1 contour de pixel pour bouger la bordure
 }
